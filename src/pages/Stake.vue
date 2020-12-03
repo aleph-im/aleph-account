f024f7c0a9c9c270f19230faab8d6733a1313f49
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2020-12-03 17:48:59 +0100

    work in progress

--- src/pages/Stake.vue
+++ src/pages/Stake.vue
@@ -3,6 +3,7 @@
     <q-table
       title="Nodes"
       :data="values"
+      :columns="columns"
       :filter="filter"
       flat
       class="bg-transparent"
@@ -13,6 +14,35 @@
             <q-icon name="search" />
           </template>
         </q-input>
+      </template>
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="name" :props="props">
+            Node-ID: {{ props.row.id }}<br />
+            {{ props.row.name }}
+          </q-td>
+          <q-td key="staked" :props="props">
+            <div class="flex-row justify-between">
+              <span>
+                {{ (props.row.staked/1000).toFixed(0) }}k
+              </span>
+              <span style="text-transform: capitalize;">{{ props.row.status }}</span>
+            </div>
+            <q-linear-progress rounded size="5px" :color="props.row.status === 'active' ? 'positive': 'inactivegradient'" :value="props.row.staked > 500000 ? 1.0 : props.row.staked/500000" />
+          </q-td>
+          <q-td key="uptime" :props="props">
+            {{ props.row.uptime }} %
+          </q-td>
+          <q-td key="creationDate" :props="props">
+            {{ props.row.creationDate.toLocaleDateString() }}
+          </q-td>
+          <q-td key="stared" :props="props">
+
+          </q-td>
+          <q-td key="actions" :props="props">
+
+          </q-td>
+        </q-tr>
       </template>
     </q-table>
   </q-page>
@@ -30,14 +60,59 @@ export default {
   data () {
     return {
       filter: '',
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'Name',
+          align: 'left',
+          field: 'nodeName',
+          sortable: true
+        },
+        {
+          name: 'staked',
+          label: 'Staked',
+          field: 'staked',
+          align: 'left'
+        },
+        {
+          name: 'uptime',
+          label: 'Up-Time',
+          field: 'uptime'
+        },
+        {
+          name: 'creationDate',
+          label: 'Creation Date',
+          field: 'creationDate'
+        },
+        {
+          name: 'stared',
+          label: '',
+          field: 'stared'
+        },
+        {
+          name: 'actions',
+          label: '',
+          align: 'right'
+        }
+      ],
       values: [
         {
-          nodeId: 'node-id-123456',
-          nodeName: '<Name>',
+          id: 'node-id-123456',
+          name: '<Name>',
           staked: 321000,
+          status: 'waiting',
+          uptime: 0,
+          creationDate: new Date(2020, 11, 18, 11, 18, 31, 255),
+          starred: false
+        },
+        {
+          id: 'node-id-123457',
+          name: '<Name>',
+          staked: 589253,
           status: 'active',
           uptime: 100,
-          creationDate: new Date(2020, 12, 18, 11, 18, 31, 255),
+          creationDate: new Date(2020, 11, 17, 10, 14, 28, 255),
           starred: false
         }
       ]
