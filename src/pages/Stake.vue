94ed022e018beaf9de0df42c018311fd1ac007f9
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2020-12-16 00:41:45 +0100

    actually working staking

--- src/pages/Stake.vue
+++ src/pages/Stake.vue
@@ -1,8 +1,9 @@
 <template>
   <q-page class="q-pa-md">
     <q-dialog v-model="createNode">
-      <CreateNode />
+      <CreateNode @done="creation_done" />
     </q-dialog>
+    {{values}}
     <q-table
       title="Nodes"
       :data="values"
@@ -18,7 +19,7 @@
               <q-icon name="search" />
             </template>
           </q-input>
-          <q-btn v-if="account && (balance_info.ALEPH >= 200000)"
+          <q-btn :disabled="!((account && (balance_info.ALEPH >= 200000))&&(user_node===null))"
             color="aleph-radial" text-color="white"
             class="q-ml-sm" icon="add" size="sm" @click="createNode = true" rounded>
             Create node
@@ -28,38 +29,40 @@
       <template v-slot:body="props">
         <q-tr :props="props">
           <q-td key="name" :props="props">
-            <span class="text-grey">Node-ID:</span> {{ props.row.id }}<br />
+            <span class="text-grey">Node-ID:</span> {{ props.row.hash.slice(-10) }}<br />
             {{ props.row.name }}
           </q-td>
-          <q-td key="staked" :props="props" width="200">
+          <q-td key="total_staked" :props="props" width="200">
             <div class="row justify-between">
               <span>
-                {{ (props.row.staked/1000).toFixed(0) }}k
+                {{ (props.row.total_staked/1000).toFixed(0) }}k
               </span>
               <span style="text-transform: capitalize;">{{ props.row.status }}</span>
             </div>
             <q-linear-progress rounded size="5px"
             :color="props.row.status === 'active' ? 'positive': 'inactive'"
-            :value="props.row.staked > 500000 ? 1.0 : props.row.staked/500000"
+            :value="props.row.total_staked > 500000 ? 1.0 : props.row.total_staked/500000"
             class="q-my-sm">
             </q-linear-progress>
-            <div class="posbadge" :style="`margin-left: calc(${(props.row.staked > 500000 ? 1.0 : props.row.staked/500000)*100}% - 28px - 16px)`">
+            <div class="posbadge" :style="`margin-left: calc(${(props.row.total_staked > 450000 ? 1.0 : props.row.total_staked/500000)*100}% - 16px)`">
               <q-badge :color="props.row.status === 'active' ? 'positive': 'inactive'"
                        text-color="white"
-                       :label="(props.row.staked > 500000 ? '+' : '') + ((props.row.staked - 500000)/1000).toFixed(0) + 'k'" />
+                       :label="(props.row.total_staked > 500000 ? '+' : '') + ((props.row.total_staked - 500000)/1000).toFixed(0) + 'k'" />
             </div>
           </q-td>
           <q-td key="uptime" :props="props">
             {{ props.row.uptime }} %
           </q-td>
-          <q-td key="creationDate" :props="props">
-            {{ props.row.creationDate.toLocaleDateString() }}
+          <q-td key="time" :props="props">
+            {{ new Date(props.row.time*1000).toLocaleDateString() }}
           </q-td>
           <q-td key="stared" :props="props">
 
           </q-td>
           <q-td key="actions" :props="props">
-
+            <q-btn size="sm" color="warning" text-color="black" v-if="account&&user_node&&(user_node.hash == props.row.hash)" @click="drop_node(props.row.hash)">drop node</q-btn>
+            <q-btn size="sm" color="warning" text-color="black" v-else-if="account&&user_stake&&(user_stake.hash == props.row.hash)" @click="unstake(props.row.hash)">unstake</q-btn>
+            <q-btn size="sm" color="aleph-radial" text-color="white" v-else :disabled="!(account&&(balance_info.ALEPH >= 10000))" @click="stake(props.row.hash)">stake</q-btn>
           </q-td>
         </q-tr>
       </template>
@@ -70,12 +73,58 @@
 <script>
 import { mapState } from 'vuex'
 import CreateNode from '../components/CreateNode'
+import { posts } from 'aleph-js'
+import store from '../store'
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+async function node_action (action, ref) {
+  await posts.submit(store.state.account.address, store.state.node_post_type,
+    {
+      tags: [action, ...store.state.tags],
+      action: action
+    },
+    {
+      api_server: store.state.api_server,
+      account: store.state.account,
+      channel: store.state.channel,
+      ref: ref
+    })
+  await sleep(3000)
+}
+
 export default {
   name: 'StakePage',
   computed: mapState({
     account: state => state.account,
     balance_info: state => state.balance_info,
-    api_server: state => state.api_server
+    api_server: state => state.api_server,
+    nodes: state => state.nodes,
+    node_post_type: 'node_post_type',
+    values (state) {
+      return state.nodes
+    },
+    user_node (state) {
+      if (state.account) {
+        for (let node of state.nodes) {
+          if (state.account.address === node.owner) {
+            console.log(node)
+            return node
+          }
+        }
+      }
+      return null
+    },
+    user_stake (state) {
+      if (state.account) {
+        for (let node of state.nodes) {
+          if (Object.keys(node.stakers).includes(state.account.address)) {
+            return node
+          }
+        }
+      }
+      return null
+    }
   }),
   components: {
     CreateNode
@@ -90,13 +139,13 @@ export default {
           required: true,
           label: 'Name',
           align: 'left',
-          field: props => props.id + ' ' + props.name,
+          field: props => props.hash + ' ' + props.name,
           sortable: true
         },
         {
-          name: 'staked',
+          name: 'total_staked',
           label: 'Staked',
-          field: 'staked',
+          field: 'total_staked',
           align: 'left'
         },
         {
@@ -105,9 +154,9 @@ export default {
           field: 'uptime'
         },
         {
-          name: 'creationDate',
+          name: 'time',
           label: 'Creation Date',
-          field: 'creationDate'
+          field: 'time'
         },
         {
           name: 'stared',
@@ -119,28 +168,55 @@ export default {
           label: '',
           align: 'right'
         }
-      ],
-      values: [
-        {
-          id: 'node-id-123456',
-          name: '<Name>',
-          staked: 321000,
-          status: 'waiting',
-          uptime: 0,
-          creationDate: new Date(2020, 11, 18, 11, 18, 31, 255),
-          starred: false
-        },
-        {
-          id: 'node-id-123457',
-          name: '<Name>',
-          staked: 589253,
-          status: 'active',
-          uptime: 100,
-          creationDate: new Date(2020, 11, 17, 10, 14, 28, 255),
-          starred: false
-        }
       ]
+      // values: [
+      //   {
+      //     id: 'node-id-123456',
+      //     name: '<Name>',
+      //     staked: 321000,
+      //     status: 'waiting',
+      //     uptime: 0,
+      //     creationDate: new Date(2020, 11, 18, 11, 18, 31, 255),
+      //     starred: false
+      //   },
+      //   {
+      //     id: 'node-id-123457',
+      //     name: '<Name>',
+      //     staked: 589253,
+      //     status: 'active',
+      //     uptime: 100,
+      //     creationDate: new Date(2020, 11, 17, 10, 14, 28, 255),
+      //     starred: false
+      //   }
+      // ]
     }
+  },
+  methods: {
+    async creation_done () {
+      this.createNode = false
+      await sleep(2000)
+      await this.update()
+    },
+    async update () {
+      await this.$store.dispatch('update_nodes')
+    },
+    async stake (node_hash) {
+      await node_action('stake', node_hash)
+      await this.update()
+    },
+    async unstake (node_hash) {
+      await node_action('unstake', node_hash)
+      console.log('updating')
+      await this.update()
+    },
+    async drop_node (node_hash) {
+      await node_action('drop-node', node_hash)
+      console.log('updating')
+      await this.update()
+    }
+  },
+  async created () {
+    await this.update()
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -19,265 +19,280 @@ Vue.use(Vuex)
  * directly export the Store instantiation
  */
 
-export default function (/* { ssrContext } */) {
-  const Store = new Vuex.Store({
-    state: {
-      erc20_address: '0x757F951a861b812F9fbD0646d9Fa73715F790E93',
-      api_server: 'https://api2.aleph.im',
-      ipfs_gateway: 'https://ipfs.io/ipfs/',
-      account: null,
-      profiles: {},
-      last_broadcast: null,
-      categories: [ // categories are hard-coded for now...
-      ],
-      pages: {},
-      menu: [],
-      notes: [],
-      notebooks: {},
-      files: [],
-      mb_per_aleph: 0.25,
-      balance_info: {
-        ALEPH: 0
-      },
-      decimals: 18,
-      channel: 'TEST',
-      ethereum_network: 'rinkeby',
-      infura_key: '4890a5bd89854916b128088119d76b50',
-      ethereum_provider: null,
-      tags: ['testnet', 'rinkeby'],
-      node_post_type: 'corechan-operation',
-      staker_post_type: 'corechan-delegation'
+// export default function (/* { ssrContext } */) {
+export default new Vuex.Store({
+  state: {
+    erc20_address: '0x757F951a861b812F9fbD0646d9Fa73715F790E93',
+    monitor_address: '0x86bfBC59a1d1D82D2596fdeB02538fDe0426faD2',
+    api_server: 'https://api2.aleph.im',
+    ipfs_gateway: 'https://ipfs.io/ipfs/',
+    account: null,
+    profiles: {},
+    last_broadcast: null,
+    categories: [ // categories are hard-coded for now...
+    ],
+    pages: {},
+    menu: [],
+    notes: [],
+    notebooks: {},
+    files: [],
+    nodes: [],
+    mb_per_aleph: 1,
+    balance_info: {
+      ALEPH: 0
     },
-    mutations: {
-      set_account (state, account) {
-        if (state.account !== account) {
-          state.notes = []
-          state.files = []
+    decimals: 18,
+    channel: 'TEST',
+    ethereum_network: 'rinkeby',
+    infura_key: '4890a5bd89854916b128088119d76b50',
+    ethereum_provider: null,
+    tags: ['testnet', 'rinkeby'],
+    node_post_type: 'corechan-operation'
+  },
+  mutations: {
+    set_account (state, account) {
+      if (state.account !== account) {
+        state.notes = []
+        state.files = []
+      }
+      state.account = account
+    },
+    set_pages (state, pages) { // TODO: handle per-page mutation
+      state.pages = pages
+    },
+    set_files (state, files) { // TODO: handle per-files mutation
+      state.files = files
+    },
+    set_notes (state, notes) {
+      state.notes = notes
+    },
+    set_notebooks (state, notebooks) {
+      state.notebooks = notebooks
+    },
+    set_nodes (state, nodes) {
+      state.nodes = nodes
+    },
+    update_note (state, new_note) {
+      for (const note of state.notes) {
+        if (note.hash === new_note.hash) {
+          Object.assign(note, new_note)
         }
-        state.account = account
-      },
-      set_pages (state, pages) { // TODO: handle per-page mutation
-        state.pages = pages
-      },
-      set_files (state, files) { // TODO: handle per-files mutation
-        state.files = files
-      },
-      set_notes (state, notes) {
-        state.notes = notes
-      },
-      set_notebooks (state, notebooks) {
-        state.notebooks = notebooks
-      },
-      update_note (state, new_note) {
-        for (const note of state.notes) {
-          if (note.hash === new_note.hash) {
-            Object.assign(note, new_note)
-          }
-        }
-      },
-      update_file (state, new_file) {
-        for (const file of state.files) {
-          if (file.hash === new_file.hash) {
-            Object.assign(file, new_file)
-          }
-        }
-      },
-      update_notebook (state, payload) {
-        // Object.assign(state.notebooks[payload.key], payload.notebook)
-        Vue.set(state.notebooks, payload.key, payload.notebook)
-      },
-      add_note (state, new_note) {
-        state.notes.unshift(new_note)
-      },
-      add_file (state, new_file) {
-        state.files.unshift(new_file)
-      },
-      add_notebook (state, payload) {
-        Vue.set(state.notebooks, payload.key, payload.notebook)
-        // state.notebooks[payload.key] = payload.notebook
-      },
-      set_menu (state, menu_items) {
-        state.menu = menu_items
-      },
-      store_profile (state, payload) {
-        state.profiles[payload.address] = payload.profile
-      },
-      set_balance_info (state, balance_info) {
-        state.balance_info = balance_info
-      },
-      set_provider (state, ethereum_provider) {
-        state.ethereum_provider = ethereum_provider
       }
     },
-    actions: {
-      async connect_provider ({ state, commit }) {
-        // Connect to INFUA
-        var infuraProvider = new providers.InfuraProvider(
-          state.ethereum_network,
-          state.infura_key)
+    update_file (state, new_file) {
+      for (const file of state.files) {
+        if (file.hash === new_file.hash) {
+          Object.assign(file, new_file)
+        }
+      }
+    },
+    update_notebook (state, payload) {
+      // Object.assign(state.notebooks[payload.key], payload.notebook)
+      Vue.set(state.notebooks, payload.key, payload.notebook)
+    },
+    add_note (state, new_note) {
+      state.notes.unshift(new_note)
+    },
+    add_file (state, new_file) {
+      state.files.unshift(new_file)
+    },
+    add_notebook (state, payload) {
+      Vue.set(state.notebooks, payload.key, payload.notebook)
+      // state.notebooks[payload.key] = payload.notebook
+    },
+    set_menu (state, menu_items) {
+      state.menu = menu_items
+    },
+    store_profile (state, payload) {
+      state.profiles[payload.address] = payload.profile
+    },
+    set_balance_info (state, balance_info) {
+      state.balance_info = balance_info
+    },
+    set_provider (state, ethereum_provider) {
+      state.ethereum_provider = ethereum_provider
+    }
+  },
+  actions: {
+    async connect_provider ({ state, commit }) {
+      // Connect to INFUA
+      var infuraProvider = new providers.InfuraProvider(
+        state.ethereum_network,
+        state.infura_key)
 
-        // Connect to Etherscan
-        var etherscanProvider = new providers.EtherscanProvider(
-          state.ethereum_network)
+      // Connect to Etherscan
+      var etherscanProvider = new providers.EtherscanProvider(
+        state.ethereum_network)
 
-        // Creating a provider to automatically fallback onto Etherscan
-        // if INFURA is down
-        var fallbackProvider = new providers.FallbackProvider([
-          infuraProvider,
-          etherscanProvider
-        ])
-        await commit('set_provider', fallbackProvider)
-      },
-      async store_account ({ state, commit }, account) {
+      // Creating a provider to automatically fallback onto Etherscan
+      // if INFURA is down
+      var fallbackProvider = new providers.FallbackProvider([
+        infuraProvider,
+        etherscanProvider
+      ])
+      await commit('set_provider', fallbackProvider)
+    },
+    async store_account ({ state, commit }, account) {
+      try {
+        LocalStorage.set('account', account)
+      } catch (e) {
+        console.warn("Can't store account")
+      }
+      await commit('set_account', account)
+    },
+    async logout ({ state, commit }) {
+      try {
+        LocalStorage.set('account', null)
+      } catch (e) {
+        console.warn("Can't store account")
+      }
+      await commit('set_account', null)
+    },
+    async update_notes ({ state, commit }, progress_callback) {
+      const result = await posts.get_posts('note', {
+        pagination: 1000,
+        addresses: [state.account.address],
+        api_server: state.api_server
+      })
+      const post_list = []
+      let i = 0
+      for (const post of result.posts) {
+        i += 1
         try {
-          LocalStorage.set('account', account)
+          if (post.content.private) {
+            await decrypt_content(post.content, ['title', 'body', 'notebook'], state.account)
+            // post.content.encrypted_title = post.content.title
+            // post.content.title = encryption.decrypt(state.account, post.content.title)
+            // post.content.encrypted_body = post.content.body
+            // post.content.body = encryption.decrypt(state.account, post.content.body)
+          }
         } catch (e) {
-          console.warn("Can't store account")
+          console.error("Can't decrypt...", e)
         }
-        await commit('set_account', account)
-      },
-      async logout ({ state, commit }) {
+        if (progress_callback !== undefined) {
+          await progress_callback(i / result.posts.length)
+        }
+        post_list.push(post)
+      }
+      commit('set_notes', post_list)
+    },
+    async update_files ({ state, commit }, progress_callback) {
+      const result = await posts.get_posts('file,folder', {
+        pagination: 10000,
+        addresses: [state.account.address],
+        api_server: state.api_server
+      })
+      let post_list = []
+      let i = 0
+      for (let post of result.posts) {
+        i += 1
         try {
-          LocalStorage.set('account', null)
+          if ((post.content.private) | (post.type === 'folder')) {
+            await decrypt_content(post.content, ['filename', 'mimetype', 'thumbnail_url'], state.account)
+          }
         } catch (e) {
-          console.warn("Can't store account")
+          console.error("Can't decrypt...", e)
         }
-        await commit('set_account', null)
-      },
-      async update_notes ({ state, commit }, progress_callback) {
-        const result = await posts.get_posts('note', {
-          pagination: 1000,
-          addresses: [state.account.address],
-          api_server: state.api_server
-        })
-        const post_list = []
-        let i = 0
-        for (const post of result.posts) {
-          i += 1
-          try {
-            if (post.content.private) {
-              await decrypt_content(post.content, ['title', 'body', 'notebook'], state.account)
-              // post.content.encrypted_title = post.content.title
-              // post.content.title = encryption.decrypt(state.account, post.content.title)
-              // post.content.encrypted_body = post.content.body
-              // post.content.body = encryption.decrypt(state.account, post.content.body)
-            }
-          } catch (e) {
-            console.error("Can't decrypt...", e)
-          }
-          if (progress_callback !== undefined) {
-            await progress_callback(i / result.posts.length)
-          }
-          post_list.push(post)
+        if (progress_callback !== undefined) {
+          await progress_callback(i / result.posts.length)
         }
-        commit('set_notes', post_list)
-      },
-      async update_files ({ state, commit }, progress_callback) {
-        const result = await posts.get_posts('file,folder', {
-          pagination: 10000,
-          addresses: [state.account.address],
-          api_server: state.api_server
-        })
-        let post_list = []
-        let i = 0
-        for (let post of result.posts) {
-          i += 1
-          try {
-            if ((post.content.private) | (post.type === 'folder')) {
-              await decrypt_content(post.content, ['filename', 'mimetype', 'thumbnail_url'], state.account)
-            }
-          } catch (e) {
-            console.error("Can't decrypt...", e)
-          }
-          if (progress_callback !== undefined) {
-            await progress_callback(i / result.posts.length)
-          }
-          post_list.push(post)
-        }
-        commit('set_files', post_list)
-      },
-      async update_balance ({ state, commit }) {
-        if (state.account !== null) {
-          if (state.account.type === 'NULS2') {
-            commit('set_balance_info', await get_nuls_balance_info(
-              state.account.address, 'https://nuls.world'
-            ))
-          } else if (state.account.type === 'ETH') {
-            let val = await get_erc20_balance(
-              state.ethereum_provider,
-              state.erc20_address,
-              state.account.address
-            )
-            val = val / (10 ** state.decimals)
-            commit('set_balance_info', {
-              ALEPH: val
-            })
-          } else if (state.account.type === 'NEO') {
-            commit('set_balance_info', await get_neo_balance_info(
-              state.account.address, 'https://api.neoscan.io'
-            ))
-          }
-        }
-      },
-      async update_notebooks ({ state, commit }, progress_callback) {
-        let notebooks = await aggregates.fetch_one(
-          state.account.address,
-          'notebooks', {
-            api_server: state.api_server
+        post_list.push(post)
+      }
+      commit('set_files', post_list)
+    },
+    async update_balance ({ state, commit }) {
+      if (state.account !== null) {
+        if (state.account.type === 'NULS2') {
+          commit('set_balance_info', await get_nuls_balance_info(
+            state.account.address, 'https://nuls.world'
+          ))
+        } else if (state.account.type === 'ETH') {
+          let val = await get_erc20_balance(
+            state.ethereum_provider,
+            state.erc20_address,
+            state.account.address
+          )
+          val = val / (10 ** state.decimals)
+          commit('set_balance_info', {
+            ALEPH: val
           })
-        if (notebooks !== null) {
-          let i = 0
-          let key_len = Object.keys(notebooks).length
-          for (let [key, item] of Object.entries(notebooks)) { // eslint-disable-line no-unused-vars
-            i += 1
-            try {
-              if (item.private) {
-                await decrypt_content(item, ['name', 'description'], state.account)
-              }
-            } catch (e) {
-              console.error("Can't decrypt...", e)
-            }
-            if (progress_callback !== undefined) {
-              await progress_callback(i / key_len)
-            }
-          }
-        } else {
-          notebooks = {}
+        } else if (state.account.type === 'NEO') {
+          commit('set_balance_info', await get_neo_balance_info(
+            state.account.address, 'https://api.neoscan.io'
+          ))
         }
-        commit('set_notebooks', notebooks)
       }
-      // async update_pages({ state, commit }) {
-      //   let pages = await fetch_one(
-      //     state.site_address, 'pages', {
-      //     'api_server': state.api_server
-      //   })
-      //   if (pages === null)
-      //     pages = {}
-
-      //   await commit('set_pages', pages)
-      // },
-      // async update_menu({ state, commit }) {
-      //   let menu_obj = await fetch_one(
-      //     state.site_address, 'menu', {
-      //     'api_server': state.api_server
-      //   })
-      //   let items = []
-      //   if ((menu_obj !== null) && menu_obj.items)
-      //     items = menu_obj.items
-
-      //   await commit('set_menu', items)
-      // }
     },
-
-    modules: {
-      // example
+    async update_notebooks ({ state, commit }, progress_callback) {
+      let notebooks = await aggregates.fetch_one(
+        state.account.address,
+        'notebooks', {
+          api_server: state.api_server
+        })
+      if (notebooks !== null) {
+        let i = 0
+        let key_len = Object.keys(notebooks).length
+        for (let [key, item] of Object.entries(notebooks)) { // eslint-disable-line no-unused-vars
+          i += 1
+          try {
+            if (item.private) {
+              await decrypt_content(item, ['name', 'description'], state.account)
+            }
+          } catch (e) {
+            console.error("Can't decrypt...", e)
+          }
+          if (progress_callback !== undefined) {
+            await progress_callback(i / key_len)
+          }
+        }
+      } else {
+        notebooks = {}
+      }
+      commit('set_notebooks', notebooks)
     },
+    async update_nodes ({ state, commit }) {
+      let corechannel = await aggregates.fetch_one(
+        state.monitor_address,
+        'corechannel', {
+          api_server: state.api_server
+        })
 
-    // enable strict mode (adds overhead!)
-    // for dev mode only
-    // strict: process.env.DEBUGGING
-    strict: false
-  })
+      let nodes = []
+      if (corechannel.nodes !== undefined) { nodes = corechannel.nodes }
+      commit('set_nodes', nodes)
+    }
+    // async update_pages({ state, commit }) {
+    //   let pages = await fetch_one(
+    //     state.site_address, 'pages', {
+    //     'api_server': state.api_server
+    //   })
+    //   if (pages === null)
+    //     pages = {}
 
-  return Store
-}
+    //   await commit('set_pages', pages)
+    // },
+    // async update_menu({ state, commit }) {
+    //   let menu_obj = await fetch_one(
+    //     state.site_address, 'menu', {
+    //     'api_server': state.api_server
+    //   })
+    //   let items = []
+    //   if ((menu_obj !== null) && menu_obj.items)
+    //     items = menu_obj.items
+
+    //   await commit('set_menu', items)
+    // }
+  },
+
+  modules: {
+    // example
+  },
+
+  // enable strict mode (adds overhead!)
+  // for dev mode only
+  // strict: process.env.DEBUGGING
+  strict: false
+})
+
+// return Store
+// }
