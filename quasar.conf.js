d95c69af16c9328dd0d5d418a21cc26b9eb866a6
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2020-12-21 14:33:48 +0100

    add vue donut chart

--- quasar.conf.js
+++ quasar.conf.js
@@ -21,7 +21,8 @@ module.exports = function (/* ctx */) {
     boot: [
 
       'i18n',
-      'axios'
+      'axios',
+      'donut'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
