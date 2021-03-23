7d89a9147f8bc121766dab97dec56acdc5858766
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2021-03-23 18:26:05 +0100

    NFT work in progress (stepper now)

--- src/components/NftSnapshot.vue
+++ src/components/NftSnapshot.vue
@@ -1,105 +1,138 @@
 <template>
-  <q-card class="infocard">
-    <div class="row">
-      <div class="col-12 col-md-5 q-pa-md">
-        <div class="text-weight-bold text-h5 q-mb-md">
-          NFT Info
-        </div>
-        <q-tabs
-        v-model="tab"
-        inline-label
-        no-caps
-        >
-          <q-tab name="url" label="From URL" />
-          <q-tab name="contract" label="From Contract Info" />
-        </q-tabs>
-        <q-tab-panels v-model="tab" class="bg-transparent">
-          <q-tab-panel name="url">
-            <q-input v-model="source_url" label="NFT marketplace URL"
+  <q-stepper
+        v-model="step"
+        header-nav
+        ref="stepper"
+        color="primary"
+        class="shadow-2 infocard"
+        animated
+      >
+    <q-step
+      :name="1"
+      title="NFT Details"
+      icon="settings"
+      :done="step > 1"
+      :header-nav="step > 1"
+    >
+      Let's try to fetch your NFT data.<br />
+      Please either copy a marketplace URL, or directly the contract address and token ID.
+      <div class="row q-gutter-md q-mt-md">
+        <div :class="'col' + (tab!=='url' ? ' disabled': '')">
+          <q-radio v-model="tab" val="url" label="From an URL" />
+            <q-input v-model="source_url" label="NFT marketplace on"
+             @click="tab='url'"
             stack-label standout class="q-my-sm" />
-            <q-btn @click="parse_url">Fetch</q-btn>
-          </q-tab-panel>
-          <q-tab-panel name="contract">
+        </div>
+        <div :class="'col' + (tab!=='contract' ? ' disabled': '')">
+          <q-radio v-model="tab" val="contract" label="From contract info" />
             <q-input v-model="nft_contract" label="Contract Address"
+            @click="tab='contract'"
             stack-label standout
             class="q-my-sm" />
             <q-input v-model="nft_index" label="Token ID"
+            @click="tab='contract'"
             stack-label standout
             class="q-my-sm" />
-            <q-btn @click="fetch_token">Fetch</q-btn>
-          </q-tab-panel>
-        </q-tab-panels>
-      </div>
-      <div class="col-12 col-md-7 q-pa-md">
-        <div class="row justify-between">
-          <div class="text-weight-bold text-h5 q-mb-md">
-            {{(nft_data && nft_data.name) ? nft_data.name : "Summary"}}
-          </div>
-          <span>
-            <q-btn color="grey" icon="close"
-            size="xs" padding="xs" class="radius-4"
-            @click="$emit('close')"/>
-          </span>
         </div>
-        <q-list v-if="nft_data">
-          <q-item class="standout" v-if="nft_data.name">
-            <q-item-section>
-              <q-item-label caption>name</q-item-label>
-              <q-item-label class="text-body2 overflow-hidden">
-                {{nft_data.name}}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item v-if="nft_data.description"  class="standout">
-            <q-item-section>
-              <q-item-label caption>Description</q-item-label>
-              <q-item-label class="text-body2 overflow-hidden">
-                {{nft_data.description}}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item v-if="nft_data.image"  class="standout">
-            <q-item-section>
-              <q-item-label caption>Image</q-item-label>
-              <img :src="nft_data.image.replace('ipfs://', 'https://ipfs.io/')" class="full-width q-my-sm" />
-            </q-item-section>
-          </q-item>
-          <q-item v-if="nft_data.animation_url"  class="standout">
-            <q-item-section>
-              <q-item-label caption>Animation</q-item-label>
-              <video :src="nft_data.animation_url.replace('ipfs://', 'https://ipfs.io/')" class="full-width q-my-sm" />
-            </q-item-section>
-          </q-item>
-          <q-item v-if="nft_data.tags"  class="standout">
-            <q-item-section>
-              <q-item-label caption>Tags</q-item-label>
-              <q-item-label class="text-body2 overflow-hidden">
-                {{nft_data.tags}}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item class="standout" v-if="nft_data.external_url && (!nft_data.external_url.includes('javascript'))">
-            <q-item-section>
-              <q-item-label caption>External URL</q-item-label>
-              <q-item-label class="text-body2 overflow-hidden">
-                <a :href="nft_data.external_url" class="text-white">{{nft_data.external_url}}</a>
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item class="standout" v-if="nft_data.attributes">
-            <q-item-section>
-              <q-item-label caption>Attributes</q-item-label>
-              <q-table :data="nft_data.attributes" class="full-width">
-              </q-table>
-            </q-item-section>
-          </q-item>
-        </q-list>
-        <q-inner-loading :showing="content_loading">
-          <q-spinner-gears size="50px" color="primary" />
-        </q-inner-loading>
       </div>
-    </div>
-  </q-card>
+
+      <q-stepper-navigation>
+        <q-btn @click="fetch_info" color="primary" label="Fetch Details" />
+      </q-stepper-navigation>
+    </q-step>
+    <q-step
+      :name="2"
+      title="Review NFT"
+      icon="create_new_folder"
+      :done="step > 2"
+      :header-nav="step > 2"
+    >
+      <div class="row q-gutter-md" v-if="nft_data">
+        <div class="col-4">
+          <q-list>
+            <q-item v-if="nft_data.image"  class="standout">
+              <q-item-section>
+                <q-item-label caption>Image</q-item-label>
+                <img :src="nft_data.image.replace('ipfs://', 'https://ipfs.io/')" class="full-width q-my-sm" />
+              </q-item-section>
+            </q-item>
+            <q-item v-else-if="nft_data.animation_url"  class="standout">
+              <q-item-section>
+                <q-item-label caption>Animation</q-item-label>
+                <video :src="nft_data.animation_url.replace('ipfs://', 'https://ipfs.io/')" class="full-width q-my-sm" />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+        <div class="col-7">
+          <q-list>
+            <q-item class="standout" v-if="nft_data.name">
+              <q-item-section>
+                <q-item-label caption>name</q-item-label>
+                <q-item-label class="text-body2 overflow-hidden">
+                  {{nft_data.name}}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item v-if="nft_data.description"  class="standout">
+              <q-item-section>
+                <q-item-label caption>Description</q-item-label>
+                <q-item-label class="text-body2 overflow-hidden">
+                  {{nft_data.description}}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item v-if="nft_data.tags"  class="standout">
+              <q-item-section>
+                <q-item-label caption>Tags</q-item-label>
+                <q-item-label class="text-body2 overflow-hidden">
+                  <q-badge v-for="tag of nft_data.tags" :key="tag" color="aleph-radial" class="q-ml-sm q-mt-sm">
+                    {{tag}}
+                  </q-badge>
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item class="standout" v-if="nft_data.external_url && (!nft_data.external_url.includes('javascript'))">
+              <q-item-section>
+                <q-item-label caption>External URL</q-item-label>
+                <q-item-label class="text-body2 overflow-hidden">
+                  <a :href="nft_data.external_url" class="text-white">{{nft_data.external_url}}</a>
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item class="standout" v-if="nft_data.attributes && nft_data.attributes.length">
+              <q-item-section>
+                <q-item-label caption>Attributes</q-item-label>
+                <q-table :data="nft_data.attributes" class="full-width">
+                </q-table>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+      </div>
+
+      <q-stepper-navigation>
+        <q-btn @click="() => { done2 = true; step = 3 }" color="primary" label="Continue" />
+        <q-btn flat @click="step = 1" color="primary" label="Back" class="q-ml-sm" />
+      </q-stepper-navigation>
+    </q-step>
+
+    <q-step
+      :name="3"
+      title="Create Snapshot"
+      icon="create_new_folder"
+      :header-nav="step > 3"
+    >
+      Try out different ad text to see what brings in the most customers, and learn how to
+      enhance your ads using features like ad extensions. If you run into any problems with
+      your ads, find out how to tell if they're running and how to resolve approval issues.
+
+      <q-stepper-navigation>
+        <q-btn color="primary" @click="done3 = true" label="Finish" />
+        <q-btn flat @click="step = 2" color="primary" label="Back" class="q-ml-sm" />
+      </q-stepper-navigation>
+    </q-step>
+  </q-stepper>
 </template>
 
 <script>
@@ -124,6 +157,7 @@ export default {
   ],
   data () {
     return {
+      step: 1,
       tab: 'url',
       source_url: '',
       nft_contract: '',
@@ -135,6 +169,13 @@ export default {
     }
   },
   methods: {
+    async fetch_info () {
+      if (this.tab === 'url') {
+        await this.parse_url()
+      } else if (this.tab === 'contract') {
+        await this.fetch_token()
+      }
+    },
     async fetch_token () {
       this.content_loading = true
       try {
@@ -156,6 +197,7 @@ export default {
         let result = await this.$axios.get(request_uri)
         this.nft_data = result.data
         this.tab = 'contract'
+        this.step = 2
       } catch {
         this.$q.notify({
           type: 'negative',
@@ -205,20 +247,28 @@ export default {
 .infocard {
   width: 900px !important;
   max-width: 80vw !important;
-  &>.row {
-    >:first-child {
-      border-right: 1px solid rgba(0,0,0,.1);
-    }
-  }
+  // &>.row {
+  //   >:first-child {
+  //     border-right: 1px solid rgba(0,0,0,.1);
+  //   }
+  // }
 }
 
 .body--dark {
   .infocard {
     background: #1d262e;
-    &>.row {
-      >:first-child {
-        border-right: 1px solid rgba(255,255,255,.1);
-      }
+    border: none;
+    // &>.row {
+    //   >:first-child {
+    //     border-right: 1px solid rgba(255,255,255,.1);
+    //   }
+    // }
+  }
+
+  .q-stepper {
+    background: #1d262e;
+    .q-stepper__tab--active, .q-stepper__tab--done {
+      color: white;
     }
   }
 }
