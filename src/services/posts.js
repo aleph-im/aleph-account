9b7057ff250d56c4b75b451ea7d63bda5128a05a
Moshe Malawach <moshe.malawach@protonmail.com>
2021-03-26 00:05:07 +0100

    working nft backup, archival supported, copying cid info too

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,28 +1,21 @@
 
-import { encrypt_content_for_self, decrypt_content } from '../services/encryption.js'
-import { aggregates, posts, encryption, store } from 'aleph-js'
+import { posts } from 'aleph-js'
 
-export async function update_post(postitem, new_values, encrypted_fields, account, api_server, channel) {
+export async function update_post (postitem, new_values, account, api_server, channel) {
   let post_content = {}
   Object.assign(post_content, postitem.content, new_values)
 
-  let unencrypted_content = {}
-  Object.assign(unencrypted_content, post_content)
-
-  if ((post_content.private === undefined)||(post_content.private)) {
-    post_content['private'] = true
-    await encrypt_content_for_self(post_content, encrypted_fields, account)
-  }
-
   let msg = await posts.submit(
     account.address, 'amend', post_content,
-    {channel: channel,
+    {
+      channel: channel,
       api_server: api_server,
       account: account,
-      ref:postitem.hash})
+      ref: postitem.hash
+    })
+  msg.content = post_content
   msg.hash = postitem.hash
   msg.original_type = postitem.original_type
-  msg.content = unencrypted_content
   msg.original_ref = postitem.original_ref
   return msg
 }
