be8e63c4051988375ed3bdba61d2eabc0aa35a25
Gabriel <gabrielelfassi@yahoo.fr>
2021-11-23 20:00:43 +0100

    Feature: Show vms on new section 'Virtual Machines'
    Adds a new library 'codemirror'

---
+++ src/components/VMTable.vue
@@ -0,0 +1,148 @@
+<template>
+  <div>
+    <div v-if="!loading">
+      <q-expansion-item
+        v-for="item of data"
+        class="overflow-hidden rounded-borders q-mb-md"
+        :key="item.item_hash"
+        :label="getProgramLabel(item)"
+        icon="computer"
+        expand-icon-class="text-white"
+        :header-class="'bg-expand text-white ' + ($q.dark.isActive?'bg-dark-40':'bg-aleph-radial')"
+        flat
+      >
+        <q-card  class="bg-card-expand rounded-borders" :bordered="!$q.dark.isActive">
+          <q-card-section horizontal>
+            <q-list class="col q-my-sm">
+              <q-item>
+                <q-item-section>
+                  <q-item-label caption>Item Hash</q-item-label>
+                  <q-item-label>
+                    {{item.item_hash}}
+                    <q-btn @click="copyToClipboard(item.item_hash)" flat round icon="content_copy" size="sm"/>
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <q-item-label caption>Direct link</q-item-label>
+                  <q-item-label color="white">
+                    <a :href="`https://aleph.sh/vm/${item.item_hash}`" target="_blank" style="color:white;">https://aleph.sh/vm/{{item.item_hash}}</a>
+                    <q-btn @click="copyToClipboard(`https://aleph.sh/vm/${item.item_hash}`)" flat round icon="content_copy" size="sm"/>
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <q-item-label caption>Size</q-item-label>
+                  <q-item-label class="text-body2 overflow-hidden">
+                    {{item.storeObj ? item.storeObj.size : 'Null' }} B
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <q-item-label caption>Date</q-item-label>
+                  <q-item-label class="text-body2 overflow-hidden">
+                    {{convertTimestamp(item.time)}}
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <q-item-label caption>Channel</q-item-label>
+                  <q-item-label>
+                    {{item.channel}}
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+
+            <q-separator vertical />
+
+            <q-card-actions vertical class="justify-start q-px-md">
+                <q-btn flat icon="play_arrow" align="left" label="Open app"
+                      @click="openApp(item)" />
+                <q-btn flat icon="link" align="left" label="View Explorer"
+                      @click="openExplorer(item)" />
+                <q-btn flat icon="upload_file" align="left" label="Download Program File"
+                      @click="downloadFile(item.storeObj.item_hash)" />
+            </q-card-actions>
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+    </div>
+    <div v-else>
+      <q-card  flat class="bg-card-expand rounded-borders" :bordered="!$q.dark.isActive">
+          <q-skeleton class="q-py-sm" type="rect" />
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { convertTimestamp } from '../helpers/utilities'
+import { copyToClipboard } from 'quasar'
+
+export default {
+  props: [
+    'account',
+    'data',
+    'loading'
+  ],
+  data () {
+    return {
+      convertTimestamp: convertTimestamp,
+      copyToClipboard: copyToClipboard,
+      filter: '',
+      columns: [
+        { name: 'channel', label: 'Channel', field: 'channel', align: 'left' },
+        { name: 'chain', label: 'Chain', field: 'chain' },
+        { name: 'item_hash', label: 'Item Hash', field: 'item_hash' },
+        { name: 'time', label: 'Time', field: 'time', sortable: true },
+        { name: 'size', label: 'Size', field: 'size', sortable: true }
+      ]
+    }
+  },
+
+  methods: {
+    openExplorer (message) {
+      const explorerURL = `https://explorer.aleph.im/address/ETH/${this.account.address}/message/PROGRAM/${message.item_hash}`
+      window.open(explorerURL, '_blank')
+    },
+
+    openApp (message) {
+      const explorerURL = `https://aleph.sh/vm/${message.item_hash}`
+      window.open(explorerURL, '_blank')
+    },
+
+    downloadFile (item_hash) {
+      const urlToSend = `http://api2.aleph.im/api/v0/storage/raw/${item_hash}`
+      var req = new XMLHttpRequest()
+      req.open('GET', urlToSend, true)
+      req.responseType = 'blob'
+      req.onload = function (event) {
+        var blob = req.response
+        var fileName = `${item_hash}.zip` // if you have the fileName header available
+        var link = document.createElement('a')
+        link.href = window.URL.createObjectURL(blob)
+        link.download = fileName
+        link.click()
+      }
+      req.send()
+    },
+
+    getProgramLabel (program) {
+      if (!('extra_fields' in program.content)) {
+        return program.item_hash
+      } else if (!('program_name' in program.content.extra_fields)) {
+        return program.item_hash
+      } else if (program.content.extra_fields.program_name.length === 0) {
+        return program.item_hash
+      }
+      return program.content.extra_fields.program_name
+    }
+  }
+}
+</script>
---
+++ src/pages/VMView.vue
@@ -0,0 +1,124 @@
+<template>
+  <q-page class="q-pa-md">
+        <!-- start: create vm dialog -->
+    <q-dialog v-model="showCreateProgram">
+      <CreateNewVM :account="account" :api_server="api_server" @created="uploadVM" />
+    </q-dialog>
+    <!-- end: create vm dialog -->
+    <div v-if="account" class="q-mb-md">
+      <div v-if="balance_info.ALEPH < 1" class="doc-note doc-note--warning">
+        <p>
+          <q-icon name="warning" size="md" />
+          You need ALEPH tokens to use this dApp section.
+        </p>
+      </div>
+    </div>
+    <div v-else class="login-info">
+      Please connect.
+    </div>
+    <div class="q-mb-md" v-if="account">
+    <div class="row justify-end">
+        <q-input standout v-model="search" @keydown.enter.prevent="reloadVM()" dense label="Search program" class="q-mr-md">
+          <template v-slot:prepend>
+          <q-icon name="search"/>
+        </template>
+        </q-input>
+        <q-btn icon="refresh" class="q-mr-md" color="aleph-radial" :disabled="loading" :loading="loading" label="Reload" @click="reloadVM()"/>
+        <q-btn icon="add" color="aleph-radial" label="Create program" @click="showCreateProgram = true" :disabled="balance_info.ALEPH < 1"/>
+    </div>
+    </div>
+    <div v-if="account">
+      <!-- start: active vm -->
+      <VMTable :data="programs" :account="account" :loading="loading" >
+      </VMTable>
+      <!-- end: actives vm -->
+    </div>
+  </q-page>
+</template>
+
+<script>
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+import { mapState } from 'vuex'
+import VMTable from '../components/VMTable'
+import { messages } from 'aleph-js'
+import CreateNewVM from '../components/CreateNewVM'
+
+export default {
+  name: 'programs',
+  components: {
+    VMTable,
+    CreateNewVM
+  },
+  computed: mapState({
+    account: state => state.account,
+    api_server: state => state.api_server,
+    balance_info: state => state.balance_info,
+    stored: 'stored'
+  }),
+  data () {
+    return {
+      loading: true,
+      programs: [],
+      search: '',
+      showCreateProgram: false,
+      id: '',
+      agentVersion: '',
+      tab: 'active'
+    }
+  },
+  methods: {
+    uploadVM (value) {
+      this.showCreateProgram = value
+      this.getMessages()
+    },
+
+    async getMessages () {
+      this.loading = true
+      var params = {
+        addresses: [this.account.address],
+        pagination: 1000,
+        message_type: 'PROGRAM'
+      }
+      if (this.search.length > 0) {
+        params.hashes = [this.search]
+      }
+      await messages.get_messages(params).then(async (response) => {
+        var programsTmp = response.messages
+        for (var i = 0; i < programsTmp.length; i++) {
+          let tx = programsTmp[i].content.code.ref
+          // retrieve store messages
+          await messages.get_messages({
+            addresses: [this.account.address],
+            hashes: [tx]
+          }).then(async (response) => {
+            let storeObj = response.messages[0].content
+            programsTmp[i].storeObj = storeObj
+          }).catch(() => {
+            this.loading = false
+          })
+        }
+        this.programs = programsTmp
+        this.loading = false
+      }).catch(() => {
+        this.loading = false
+      })
+    },
+
+    async reloadVM () {
+      await this.getMessages()
+    }
+  },
+  watch: {
+    account (account) {
+      this.$store.dispatch('update_stored')
+      this.getMessages()
+    }
+  },
+  async mounted () {
+    this.getMessages()
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
